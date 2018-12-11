b5f39827dded7ef1edcac32ce86b037d3c93e315
Mascot Ayere <ayeremascot@gmail.com>
2018-12-11 21:25:23 +0100

    basic react router beta

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -2,14 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
+class Hello extends react.Component {
+  render() { return <div>Hello</div>}
+}
+
+class GoodBye extends react.Component {
+  render() { return <div>GoodBye!</div>}
+}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+       <div>
+           <Route path="/hello" Component={Hello} />
+           <Route path="/GoodBye" Component={GoodBye} />
+       </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
